bb9f2e019523ae0d89843743eb4b9bc2484db189
Gavin Luo <luoxiangyu528@gmail.com>
2019-08-08 00:06:22 -0400

    Initial commit, set up basic character movement and scene

---
+++ Assets/Scripts/CameraRig.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRig : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The length of the rig, the longer the rig, the further the camera to the object.")]
+    float _rigLength = 10f;
+
+    [SerializeField]
+    [Tooltip("The object which the camera follows.")]
+    GameObject _attachedObject;
+
+    [SerializeField]
+    [Tooltip("Offset of the rig anchor point on the object from the center of the object.")]
+    Vector3 _anchorPointOffset = Vector3.zero;
+
+    [SerializeField]
+    [Tooltip("How smooth the camera response when the object moves.")]
+    [Range(0, 1)] float _cameraMoveResponse = 0.5f;
+
+    [SerializeField]
+    [Tooltip("The speed of rotation.")]
+    float _rotationSpeed = 10f;
+
+    [SerializeField]
+    [Range(0, 1)]
+    float _cameraRotateResponse = 0.5f; 
+
+    Camera _mainCamera;
+    Vector3 _eularAngles = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mainCamera = GetComponentInChildren<Camera>();
+
+        // make the camera 10 units back from the origin.
+        _mainCamera.transform.localPosition = new Vector3(0, 0, -_rigLength);
+
+        Move();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+        CheckForGround();
+    }
+
+    private void CheckForGround()
+    {
+        var onlyGroundLayerMask = 1 << LayerMask.NameToLayer("Ground");
+
+        var direction = _mainCamera.transform.position - transform.position;
+        if (Physics.Raycast(transform.position, direction, out RaycastHit hitInfo, _rigLength, onlyGroundLayerMask))
+        {
+
+            _mainCamera.transform.localPosition = new Vector3(0, 0, -hitInfo.distance + 1);
+        }
+        else
+        {
+            _mainCamera.transform.localPosition = new Vector3(0, 0, -_rigLength);
+        }
+    }
+
+    private void Move()
+    {
+        transform.position = Vector3.Lerp(transform.position, _attachedObject.transform.position + _anchorPointOffset, _cameraMoveResponse);
+    }
+
+    private void Rotate()
+    {
+        Vector3 rotateAmount = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+        _eularAngles += rotateAmount * _rotationSpeed;
+        _eularAngles.x = Mathf.Clamp(_eularAngles.x, -45f, 75f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(_eularAngles), _cameraRotateResponse);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float _speed = 0.3f;
+    [SerializeField] float _gravity = 9.8f;
+    [SerializeField] CameraRig _rig; // Camera rig determines the direction of the movement.
+
+    CharacterController _characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (movement.magnitude > 0.0f)
+        {
+            var direction = Quaternion.Euler(0, _rig.transform.eulerAngles.y, 0);
+            movement = direction * movement * _speed;
+            _characterController.Move(movement);
+            var yAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yAngle, 0), 0.5f);
+
+        }
+    }
+}
