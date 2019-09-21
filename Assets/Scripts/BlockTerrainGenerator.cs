aeab904ef94d169504c0478df9ad472698d46552
Gavin Luo <luoxiangyu528@gmail.com>
2019-09-20 22:56:04 -0400

    Add terrain generator

---
+++ Assets/Scripts/BlockTerrainGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockTerrainGenerator : MonoBehaviour
+{
+    public GameObject landBlock;
+    public int landBlockSize = 10;
+    public int numBlocksOneSide = 10;
+    public Vector3 landStartingPosition = new Vector3(-100, -100, -100);
+    public int maxHeight = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        GenerateTerrain();
+    }
+
+    void GenerateTerrain()
+    {
+        int[][] heightMap = new int[numBlocksOneSide][];
+
+        for (int i = 0; i < numBlocksOneSide; i++)
+        {
+            heightMap[i] = new int[numBlocksOneSide];
+            for (int j = 0; j < numBlocksOneSide; j++)
+            {
+                var height = Random.Range(1, maxHeight);
+                heightMap[i][j] = height;
+                for (int k = 0; k < height; k++)
+                {
+                    Vector3 position = landStartingPosition + landBlockSize * new Vector3(i, k, j);
+                    var block = Instantiate(landBlock, position, Quaternion.identity);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float cameraSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouseMovement= new Vector2(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"));
+        Vector3 eulerAngles = transform.rotation.eulerAngles;
+        eulerAngles.x -= mouseMovement.x;
+        eulerAngles.y += mouseMovement.y;
+        transform.rotation = Quaternion.Euler(eulerAngles);
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movement = transform.rotation * movement;
+        transform.position += movement * Time.deltaTime * cameraSpeed;
+    }
+}
