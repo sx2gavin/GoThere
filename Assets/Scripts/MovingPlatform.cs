a00679b1a23bef9ae9f0d8f9381a3d64ff1c4c93
Gavin Luo <luoxiangyu528@gmail.com>
2019-10-04 08:33:21 -0400

    Change Character Controller to rigidbody and collider

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -13,17 +13,18 @@ public class MovingPlatform : MonoBehaviour
 
     private Transform currentDestination;
     private Transform currentDeparture;
+    private float distance;
+    private bool isPausing = false;
     // Start is called before the first frame update
     void Start()
     {
         currentDestination = endPosition;
         currentDeparture = startPosition;
-        StartCoroutine(MoveBackAndForth());
+        distance = Vector3.Distance(startPosition.position, endPosition.position);
     }
 
     private IEnumerator MoveBackAndForth()
     {
-        var distance = Vector3.Distance(startPosition.position, endPosition.position);
         while (true)
         {
             var currentDistance = Vector3.Distance(platform.transform.position, currentDeparture.position);
@@ -47,5 +48,31 @@ public class MovingPlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isPausing)
+        {
+            return;
+        }
+
+        var currentDistance = Vector3.Distance(platform.transform.position, currentDeparture.position);
+        if (currentDistance < distance)
+        {
+            var direction = currentDestination.position - platform.transform.position;
+            var moveDistance = direction.normalized * movingSpeed * Time.deltaTime;
+            platform.transform.position += moveDistance;
+        }
+        else
+        {
+            var temp = currentDeparture;
+            currentDeparture = currentDestination;
+            currentDestination = temp;
+            StartCoroutine(Pause());
+        }
+    }
+
+    IEnumerator Pause()
+    {
+        isPausing = true;
+        yield return new WaitForSeconds(pauseDuration);
+        isPausing = false;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody), typeof(Collider))]
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 20f;
@@ -14,13 +15,15 @@ public class PlayerMovement : MonoBehaviour
     [Tooltip("Camera rig determines the direction of the movement.")]
     public CameraRig rig;
 
-    CharacterController characterController;
+    new Rigidbody rigidbody;
     float currentVerticalSpeed;
     bool movable = true;
+    bool isClimbing = false;
+    bool isGrounded = true;
     // Start is called before the first frame update
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -31,16 +34,12 @@ public class PlayerMovement : MonoBehaviour
             return;
         }
 
-        if (Input.GetAxis("Jump") > 0 && characterController.isGrounded)
+        if (Input.GetAxis("Jump") > 0 && isGrounded)
         {
             Jump(jumpSpeed);
         }
-        else
-        {
-            Fall();
-        }
 
-        Move();
+        Walk();
     }
 
     public void KnockBack(Vector3 direction)
@@ -51,36 +50,25 @@ public class PlayerMovement : MonoBehaviour
     private IEnumerator KnockBackCoroutine(Vector3 direction)
     {
         movable = false;
-        Jump(knockBackHeight);
-        while (!characterController.isGrounded)
+        // Jump(knockBackHeight);
+        while (!isGrounded)
         {
             var movementThisFrame = new Vector3(direction.x, 0, direction.z) * knockBackSpeed * Time.deltaTime;
-            characterController.Move(movementThisFrame);
+            // characterController.Move(movementThisFrame);
+            transform.localPosition += movementThisFrame;
             yield return new WaitForEndOfFrame();
         }
         movable = true;
     }
 
+
     private void Jump(float jumpSpeed)
     {
         currentVerticalSpeed = jumpSpeed;
-        characterController.Move(new Vector3(0, currentVerticalSpeed * Time.deltaTime, 0));
+        rigidbody.AddForce(new Vector3(0, jumpSpeed, 0), ForceMode.Impulse);
     }
 
-    private void Fall()
-    {
-        if (characterController.isGrounded)
-        {
-            currentVerticalSpeed = 0.0f;
-        }
-        else
-        {
-            currentVerticalSpeed -= gravity * Time.deltaTime;
-            characterController.Move(new Vector3(0, currentVerticalSpeed * Time.deltaTime, 0));
-        }
-    }
-
-    private void Move()
+    private void Walk()
     {
         if (movable && Time.timeScale > 0.0f)
         {
@@ -89,7 +77,7 @@ public class PlayerMovement : MonoBehaviour
             {
                 var direction = Quaternion.Euler(0, rig.transform.eulerAngles.y, 0);
                 movement = direction * movement * speed * Time.deltaTime;
-                characterController.Move(movement);
+                rigidbody.AddForce(movement, ForceMode.VelocityChange);
                 var yAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yAngle, 0), 0.5f);
                 animator.SetFloat("Speed", 1.0f);
@@ -102,6 +90,15 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             animator.SetFloat("Speed", 0.0f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var ladder = other.GetComponent<Ladder>();
+        if (ladder != null)
+        {
+            isClimbing = true;
         }
     }
 }
