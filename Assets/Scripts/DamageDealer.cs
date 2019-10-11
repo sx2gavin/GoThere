643cdebb2aa3feb0e30314d16dc1ba5a198af1fb
Gavin Luo <luoxiangyu528@gmail.com>
2019-10-10 23:50:00 -0400

    Update PlayerMovement to better deal with spike damage knock back, add implementation for ladder

--- Assets/Scripts/DamageDealer.cs
+++ Assets/Scripts/DamageDealer.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class DamageDealer : MonoBehaviour
 {
     public int damage = 1;
+
+    private Collider collider;
+
+    private void Start()
+    {
+        collider = GetComponent<Collider>();    
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,9 +24,20 @@ public class DamageDealer : MonoBehaviour
             if (tookDamage)
             {
                 var player = other.GetComponent<PlayerMovement>();
-                var knockBackDirection = (other.transform.position - transform.position).normalized;
-                player.KnockBack(knockBackDirection);
+                var otherCenter = other.bounds.center;
+                var myCenter = collider.bounds.center;
+                var direction = otherCenter - myCenter;
+                direction.Normalize();
+                direction += Vector3.up;
+                direction.Normalize();
+                player.KnockBack(direction);
             }
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        collider = GetComponent<Collider>();
+        Gizmos.DrawSphere(collider.bounds.center, 0.1f);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,25 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody), typeof(Collider))]
 public class PlayerMovement : MonoBehaviour
 {
+    enum PlayerState
+    {
+        Grounded,
+        Airborne,
+        KnockBack,
+        Climb
+    }
+
     public float speed = 20f;
     public float additionalGravityForce = 10f;
     public float jumpSpeed = 30.0f;
-    public float knockBackHeight;
-    public float knockBackSpeed;
-    public float groundCheckDistance = 0.1f;
+    public float knockBackForce = 10.0f;
+    public float groundCheckDistance = 0.01f;
+    public float climbingSpeed = 10.0f;
     public Animator animator;
 
     [Tooltip("Camera rig determines the direction of the movement.")]
     public CameraRig rig;
 
     new Rigidbody rigidbody;
-    bool movable = true;
-    bool isClimbing = false;
-    bool isGrounded = true;
+    PlayerState playerState = PlayerState.Grounded;
 
     // Start is called before the first frame update
     void Start()
@@ -30,55 +37,96 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (rig == null)
+        if (rig == null || Time.timeScale <= 0.0f)
         {
             return;
         }
 
-        CheckGroundState();
-        Airborne(jumpSpeed);
-        Walk();
+        CheckIfGrounded();
+
+        switch (playerState)
+        {
+            case PlayerState.Grounded:
+                Move(speed);
+                HandleJump(jumpSpeed);
+                break;
+            case PlayerState.Airborne:
+                Move(speed);
+                Airborne();
+                break;
+            case PlayerState.KnockBack:
+                Airborne();
+                break;
+            case PlayerState.Climb:
+                Climbing();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Climbing()
+    {
+        var upDown = Input.GetAxis("Vertical") * climbingSpeed;
+        rigidbody.velocity = Vector3.up * upDown;
+        if (CheckIfGrounded() == true)
+        {
+            playerState = PlayerState.Grounded;
+        }
+    }
+
+    private bool CheckIfGrounded()
+    {
+        int layerMask = 0;
+        layerMask = ~layerMask; // check ground state with everything
+        return Physics.Raycast(transform.position + Vector3.up * 0.1f, Vector3.down, out RaycastHit hitInfo, 0.1f + groundCheckDistance, layerMask, QueryTriggerInteraction.Ignore);
     }
 
     public void KnockBack(Vector3 direction)
     {
-        var knockBackDirection = direction * knockBackSpeed;
-        rigidbody.AddForce(knockBackDirection, ForceMode.Impulse);
+        playerState = PlayerState.KnockBack;
+        var knockBackDirection = direction * knockBackForce;
+        LiftPlayerOffGround();
+        rigidbody.velocity = knockBackDirection;
     }
 
-
-    private void Airborne(float jumpSpeed)
+    private void HandleJump(float jumpSpeed)
     {
-        if (isGrounded && Input.GetAxis("Jump") > 0.0f)
+        // Normal jump
+        if (playerState == PlayerState.Grounded && Input.GetAxis("Jump") > 0.0f)
         {
             var newVelocity = rigidbody.velocity;
             newVelocity.y = jumpSpeed;
             rigidbody.velocity = newVelocity;
+            playerState = PlayerState.Airborne;
         }
-        else
+
+        // Cliff jump
+        if (CheckIfGrounded() == false)
         {
-            rigidbody.AddForce(Vector3.down * additionalGravityForce);
+            playerState = PlayerState.Airborne;
         }
     }
 
-    private void Walk()
+    private void Airborne()
     {
-        Vector3 movement = Vector3.zero;
-        if (movable && Time.timeScale > 0.0f)
+        rigidbody.AddForce(Vector3.down * additionalGravityForce);
+        if (CheckIfGrounded())
         {
-            movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            if (movement.magnitude > 0.0f)
-            {
-                var direction = Quaternion.Euler(0, rig.transform.eulerAngles.y, 0);
-                movement = direction * movement * speed;
-                var yAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yAngle, 0), 0.5f);
-                animator.SetFloat("Speed", 1.0f);
-            }
-            else
-            {
-                animator.SetFloat("Speed", 0.0f);
-            }
+            playerState = PlayerState.Grounded;
+        }
+    }
+
+    private void Move(float speed)
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (movement.magnitude > 0.0f)
+        {
+            var direction = Quaternion.Euler(0, rig.transform.eulerAngles.y, 0);
+            movement = direction * movement * speed;
+            var yAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yAngle, 0), 0.5f);
+            animator.SetFloat("Speed", 1.0f);
         }
         else
         {
@@ -94,22 +142,34 @@ public class PlayerMovement : MonoBehaviour
         var ladder = other.GetComponent<Ladder>();
         if (ladder != null)
         {
-            isClimbing = true;
+            playerState = PlayerState.Climb;
+            var ladderCenter = other.bounds.center;
+            transform.position = new Vector3(ladderCenter.x, transform.position.y, ladderCenter.z);
+            LiftPlayerOffGround();
         }
     }
 
-    private void CheckGroundState()
+    private void OnTriggerStay(Collider other)
     {
-        int layerMask = 0;
-        layerMask = ~layerMask; // check ground state with everything
-        if (Physics.Raycast(transform.position + Vector3.up * 0.1f, Vector3.down, out RaycastHit hitInfo, groundCheckDistance, layerMask, QueryTriggerInteraction.Ignore))
+        var ladder = other.GetComponent<Ladder>();
+        if (ladder != null && playerState != PlayerState.Grounded)
         {
-            isGrounded = true;
+            playerState = PlayerState.Climb;
         }
-        else
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var ladder = other.GetComponent<Ladder>();
+        if (ladder != null)
         {
-            isGrounded = false;
+            playerState = PlayerState.Airborne;
         }
+    }
+
+    private void LiftPlayerOffGround()
+    {
+        transform.Translate(Vector3.up * (groundCheckDistance + 0.001f));
     }
 
     private void OnDrawGizmosSelected()
