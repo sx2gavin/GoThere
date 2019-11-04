f8d4219ab239c8ebe0beb9799c5f5802af69202d
Gavin Luo <luoxiangyu528@gmail.com>
2019-11-03 23:16:04 -0500

    Add footsteps

---
+++ Assets/Scripts/FootSteps.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class FootSteps : MonoBehaviour
+{
+    public AudioClip[] footStepSamples;
+    private AudioSource audioSource;
+
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayRandomFootStepSample()
+    {
+        int sampleIndex = Random.Range(0, footStepSamples.Length - 1);
+
+        audioSource.PlayOneShot(footStepSamples[sampleIndex]);
+    }
+}
