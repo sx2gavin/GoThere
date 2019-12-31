50808f46280ccb309daa7ae82ca86d761b5ef75a
Gavin Luo <luoxiangyu528@gmail.com>
2019-12-31 12:09:45 -0500

    Extract materials from models and convert them into LWRP materials.

---
+++ Assets/Scripts/MovingSpikes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class MovingSpikes : MonoBehaviour
+{
+    public float idleTime = 1.0f;
+    public float outTime = 1.0f;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(MoveSpikes());
+    }
+
+    private IEnumerator MoveSpikes()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(idleTime);
+            animator.SetBool("IsOut", true);
+            yield return new WaitForSeconds(outTime);
+            animator.SetBool("IsOut", false);
+        }
+    }
+}
