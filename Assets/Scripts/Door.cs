1fa0e008052021f0d3c6153ba0733dcaecf258fa
Gavin Luo <luoxiangyu528@gmail.com>
2020-01-06 09:12:05 -0500

    Add Level 3 and Level 4

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Door : SwitchTarget
 {
+    public GameObject doorModel;
+
     override public void On()
     {
         base.On();
-        transform.localRotation = Quaternion.Euler(0, 90, 0);
+        if (doorModel)
+        {
+            doorModel.transform.localRotation = Quaternion.Euler(0, 90, 0);
+        }
     }
     
     override public void Off()
     {
         base.Off();
-        transform.localRotation = Quaternion.Euler(0, 0, 0);
+        if (doorModel)
+        {
+            doorModel.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
